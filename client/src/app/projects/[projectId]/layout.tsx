702dee537f45e73e7fda680c3ed5e35e216c0d10
oniooo-dev <melody@oniooo.com>
2025-06-30 08:55:01 -0400

    wfmiwnfwi

---
+++ client/src/app/projects/[projectId]/layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { EditorProvider } from '@/contexts/EditorContext'
+import { AssetsProvider } from '@/contexts/AssetsContext'
+import { PlaybackProvider } from '@/contexts/PlaybackContext'
+import { ZoomProvider } from '@/contexts/ZoomContext'
+import { CaptionsProvider } from '@/contexts/CaptionsContext'
+
+import { AudioProvider } from '@/contexts/AudioContext'
+
+interface LayoutProps {
+    children: ReactNode
+}
+
+export default function ProjectLayout({ children }: LayoutProps) {
+    return (
+            <EditorProvider>
+                <AssetsProvider>
+                    <CaptionsProvider>
+                        <AudioProvider>
+                            <PlaybackProvider>
+                                <ZoomProvider>
+                                    {children}
+                                </ZoomProvider>
+                            </PlaybackProvider>
+                        </AudioProvider>
+                    </CaptionsProvider>
+                </AssetsProvider>
+            </EditorProvider>
+    )
+} 
